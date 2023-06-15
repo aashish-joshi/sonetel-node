28b048958892fa3e69ba9db802db40fd312d0f9b
aashish-joshi <14803874+aashish-joshi@users.noreply.github.com>
2023-06-15 11:25:39 +0530

    remove console logs

--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,24 @@ export class Sonetel {
     password: string;
 
     constructor(username: string, password: string) {
-        console.log('Sonetel constructor');
         this.username = username;
         this.password = password;
     }
 
     generateToken(refresh: boolean = false) {
         if (refresh) {
-            console.log('Sonetel generateToken refresh');
             return 'Sonetel generateToken refresh'
         } else {
-            console.log('Sonetel generateToken');
             return 'Sonetel generateToken'
         }
     }
 
     getAccount() {
-        console.log('Sonetel getAccount');
         return 'Sonetel getAccount'
     }
 
     getUsers(){
-        console.log('Sonetel getUers');
         return 'Sonetel getUers'
     }
 
 }
-
-// export default Sonetel;
